01fac3cb709dcecd790fbe2940b64ef520061ee9
iTwiixZ <alexis.champion22@gmail.com>
2022-04-11 11:41:56 +0200

    Premier commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const exp = require('constants');
+const express = require('express');
+const app     = express();
+const path    = require('path');
+
+
+// CORS
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use(express.json());
+
+
+module.exports = app;
