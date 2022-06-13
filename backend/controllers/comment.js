09fecdecc1725bc19c3ada7e6fd0a573273241af
iTwiixZ <alexis.champion22@gmail.com>
2022-06-13 13:30:07 +0200

    Update comments

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,5 +1,4 @@
 const models = require('../models');
-const { createPost } = require('./post');
 const jwt = require('jsonwebtoken');
 
 const decodeUid = (authorization) => {
@@ -12,17 +11,38 @@ const decodeUid = (authorization) => {
 };
 // Méthode pour créer un commentaire 
 
-exports.createComment = (res, req , next) => {
-    
-    const user    = decodeUid(req.headers.authorization):
-    const content = req.body.content;
-    const postId  = req.body.postId;
-    const comment  ={
-        userId: user.id,
-        content: content.content,
-        dateAdd: Date.now(),
-        postId: postId
-    }
+exports.createComment = (req, res, next) => {
+        const user = decodeUid(req.headers.authorization);
+        const content = req.body.content;
+        const dateAdd = Date.now();
+        const postId = req.body.postId;
+        const comment = {
+            content: content,
+            userId: user.id,
+            postId: postId,
+            dateAdd: dateAdd
+        }
+
+        if ( content == null) {
+            return res.status(400).json({ error:'invalid parameter'});
+        }
+
+        console.log(comment);
+
+        models.Comment.create(comment)
+        .then(result => {
+            res.status(201).json({
+                message:'Comment created successfully !',
+                comment: result
+            });
+        })
+
+        .catch(error => {
+            res.status(500).json({
+                message: 'Something went wrong !',
+                error: error
+            })
+        });
 };
 
 // Supprimer un commentaire
@@ -38,4 +58,20 @@ exports.deleteComment = (req, res, next) => {
     }
     )
     .catch((error) => {res.status(400).json({ error: error, message: "Une erreur est survenue" })});
-}
+};
+
+// Récupérer tous les commentaires
+
+exports.getComments = (req, res, next) => {
+    models.Comment.findAll({
+        where: { postId: req.params.postId },
+        order: [['id', 'DESC']], 
+    }).then(result => {
+        res.status(200).json(result);
+    }).catch(error => {
+        res.status(500).json({
+            message: 'Something went wrong'
+        });
+    });
+
+};
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router  = express.Router();
 const comment = require('../controllers/comment');
 const auth    = require('../middleware/auth');
-const multer  = require('../middleware/multer');
+
 
 
     //Comments routes
-    router.post('/new', auth, multer, comment.createComment);
-    // router.get('/getComments/:postId', auth, multer, comment.getComments);
-    // router.get('/getAll', auth, multer, comment.getAllComment);
-    router.delete('/delete/:id',auth, comment.deleteComment );
+    router.post('/new',                auth, comment.createComment);
+    router.get('/getComments/:postId', auth, comment.getComments);
+    router.delete('/delete/:id',       auth, comment.deleteComment );
 
 
 module.exports = router;
