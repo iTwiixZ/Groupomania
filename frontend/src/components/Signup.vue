7a2878c1909e154be6e8773f742983c44aa8253c
iTwiixZ <alexis.champion22@gmail.com>
2022-06-24 10:28:46 +0200

    Design signup page

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="container d-flex justify-content-center align-items-center flex-column">
             <img class="w-50" src="../assets/icon-left-font.svg" alt="Groupomania">
-        <h1 class="text-center">Ne perds pas de temps et viens nous rejoindres ! </h1>
-        <form>
-        <div class="form-group ">
+        <h1 class="text-center mt-3">Ne perds pas de temps et viens nous rejoindres ! </h1>
+        <form class="container mt-5">
+        <div class="form-group">
           <input class="form-control" v-model='name' type='name' placeholder="Nom d'utilisateur: entre 3 et 15 caractères" size="50" required >
-          
-        </div>
-        <div class="form-group ">
+          </div>
+        <div class="form-group mt-2">
           <input class="form-control" v-model='email' type='email' placeholder="Email" size="50" required aria-label="Email de connection">
         </div>
-        <div class="form-group align-items-center">
+        <div class="form-group align-items-center mt-2">
           <input class="form-control" v-model='password' type='password' placeholder="Mot de passe" size="50" required>
          </div>
           <span class="form-text">
@@ -18,7 +17,7 @@
         </span>
       </form>  
       <router-link to="/">
-        <button @click="signup()" type="submit" class="btn btn-secondary mt-2" id="login" aria-label="S'inscrire" formaction="" formenctype="application/x-www-form-urlencoded">S'inscrire</button>
+        <button @click="signup()" type="submit" class="btn  mt-5" id="login" aria-label="S'inscrire" formaction="" formenctype="application/x-www-form-urlencoded">S'inscrire</button>
       </router-link>
     </div>
 </template>
