82c2078f521b29ea59541cbea0fbf5c8b038c41c
iTwiixZ <alexis.champion22@gmail.com>
2022-07-05 11:56:23 +0200

    Prettier

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,23 +1,21 @@
-const multer = require('multer');
+const multer = require("multer");
 
-const MIME_TYPES = { 
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png',
-    'image/gif': 'gif'
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/gif": "gif",
 };
 
-const storage = multer.diskStorage({ 
-    destination: (req, file, callback) => { 
-        callback(null, 'images')
-    },
-    filename: (req, file, callback) => { 
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype]; 
-        callback(null, name + Date.now() + '.' + extension);
-    }
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
 });
 
-
-
-module.exports = multer({ storage : storage }).single('media');
+module.exports = multer({ storage: storage }).single("media");
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const router  = express.Router();
-const posts   = require('../controllers/post');
-const auth    = require('../middleware/auth');
-const multer  =  require('../middleware/multer');
+const express = require("express");
+const router = express.Router();
+const posts = require("../controllers/post");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer");
 
-
-
-    //Posts routes
-    router.post('/new',   auth, multer, posts.createPost);
-    router.get('/getOne', auth,multer, posts.getOnePost);
-    router.get('/getAll', auth, multer, posts.getAllPosts);
-    router.delete('/:id', auth, multer, posts.deletePost);
-   
-
+//Posts routes
+router.post("/new", auth, multer, posts.createPost);
+router.get("/getOne", auth, multer, posts.getOnePost);
+router.get("/getAll", auth, multer, posts.getAllPosts);
+router.delete("/:id", auth, multer, posts.deletePost);
 
 module.exports = router;
